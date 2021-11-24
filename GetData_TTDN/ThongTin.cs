cc99aa163af03167daa6b5c3f025ee7f8ec66c65
quangtm122 <quangtranht22@gmail.com>
2021-11-24 18:08:17 +0700

    first commit

---
+++ GetData_TTDN/ThongTin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetData_TTDN
+{
+    class ThongTin
+    {
+
+        public string MT_Name { get; set; }
+        public string MT_HT { get; set; }
+        public string MT_CSLN { get; set; }
+        public string MT_TK { get; set; }
+        public string MT_SLN { get; set; }
+        public string Gio_Name { get; set; }
+        public string Gio_HT { get; set; }
+        public string Gio_CSLN { get; set; }
+        public string Gio_TK { get; set; }
+        public string Gio_SLN { get; set; }
+        public string SK_Name { get; set; }
+        public string SK_HT { get; set; }
+        public string SK_CSLN { get; set; }
+        public string SK_TK { get; set; }
+        public string SK_SLN { get; set; }
+        public string Tong_Name { get; set; }
+        public string Tong_HT { get; set; }
+        public string Tong_CSLN { get; set; }
+        public string Tong_TK { get; set; }
+        public string Tong_SLN { get; set; }
+
+
+
+    }
+}
